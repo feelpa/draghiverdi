ec1ac556c47f86bd9bfcbace830e3879bf2822e6
feelpa <francesco.filpa@gmail.com>
2021-09-06 13:52:36 +0200

    added direction edited marker

--- res/js/map.js
+++ res/js/map.js
@@ -5,9 +5,33 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
-L.control.locate(initialZoomLevel = 16).addTo(map);
+L.control.locate(initialZoomLevel = 15).addTo(map);
 
+/*
 var geojsonFeatures = new L.GeoJSON.AJAX('/res/data/vedovelle.geojson');
 geojsonFeatures.on('data:loaded', function(){
     geojsonFeatures.addTo(map);
 });
+*/
+
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ffcc00ff",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.9
+};
+
+function onEachFeature(feature, layer) {
+    let url = 'http://www.google.com/maps/place/' + feature.properties.LAT_Y_4326 + ',' + feature.properties.LONG_X_4326;
+    let popupContent = '<a href="' + url + '">Apri in Google Maps</a>';
+    layer.bindPopup(popupContent);
+}
+
+L.geoJSON(data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    },
+    onEachFeature: onEachFeature
+}).addTo(map);
