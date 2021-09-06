d95f55e3a792e1d6f67d0cafbdea75abc4234f0c
feelpa <francesco.filpa@gmail.com>
2021-09-06 11:33:35 +0200

    zoom level correction

--- res/js/map.js
+++ res/js/map.js
@@ -1,11 +1,13 @@
 var map = L.map('map').setView({lon: 9.19, lat: 45.466944}, 14);
     
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 24,
+    maxZoom: 19,
     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
-L.control.locate().addTo(map);
+L.control.locate({
+    keepCurrentZoomLevel = true
+}).addTo(map);
 
 var geojsonFeatures = new L.GeoJSON.AJAX('/res/data/vedovelle.geojson');
 geojsonFeatures.on('data:loaded', function(){
